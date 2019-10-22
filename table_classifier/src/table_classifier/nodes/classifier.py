4482ba8e36111a1281d2738ed697f4917af45e37
samV2 <sambit@nirvedacognition.ai>
2019-10-22 16:01:07 +0530

    adding capping and normalize bounding points

--- table_classifier/src/table_classifier/nodes/classifier.py
+++ table_classifier/src/table_classifier/nodes/classifier.py
@@ -2,6 +2,9 @@ import logging
 from typing import Any, Dict, List, Tuple
 from sklearn.model_selection import train_test_split
 
+
+from PIL import Image
+import io
 import numpy as np
 import pandas as pd
 import base64
@@ -21,6 +24,57 @@ os.environ['PYTHONPATH'] += ':/content/kedro_tf/table_classifier/src/table_class
                             '/models/research/slim/'
 
 
+# def jsonl_image_csv(data: List,
+#                     jsonl_image_store_image_path: str
+#                     ) -> pd.DataFrame:
+#
+#     image_info_list = []
+#     cwd = os.getcwd()
+#     logging.info("storing images at : {}".format(jsonl_image_store_image_path))
+#     for annotations in data:
+#         imgdata = base64.b64decode(annotations['image'].split(',')[1])
+#         image_path = os.path.join(cwd,
+#                                   jsonl_image_store_image_path,
+#                                   annotations['text'].split('.')[0] + '.jpg')
+#         with open(image_path, 'wb') as f_image:
+#             f_image.write(imgdata)
+#         height, width = annotations['height'], annotations['width']
+#         if 'spans' in annotations:
+#             if annotations['answer'] == 'accept':
+#                 for span in annotations['spans']:
+#                     span['label'] = span['label'].replace('\ufeff', '')
+#                     points = span['points']
+#                     value = (image_path, width, height, span['label'],
+#                              int(points[0][0]),
+#                              int(points[0][1]),
+#                              int(points[2][0]),
+#                              int(points[2][1]))
+#                     image_info_list.append(value)
+#             else:
+#                 pass
+#
+#     logging.info("Number of docs : {}".format(str(len(image_info_list))))
+#     column_name = ["filename", "width", "height", "class", "xmin", "ymin", "xmax", "ymax"]
+#     image_info_df = pd.DataFrame(image_info_list, columns=column_name)
+#     return image_info_df
+
+
+def _create_dir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    else:
+        print("Directory {} already  exists".format(path))
+
+
+def b64_uri_to_bytes(b64_uri):
+    if b64_uri.startswith("data"):
+        imgstring = b64_uri.split(',')[1]
+        imgdata = base64.b64decode(imgstring)
+        return imgdata
+    else:
+        return None
+
+
 def jsonl_image_csv(data: List,
                     jsonl_image_store_image_path: str
                     ) -> pd.DataFrame:
@@ -29,23 +83,59 @@ def jsonl_image_csv(data: List,
     cwd = os.getcwd()
     logging.info("storing images at : {}".format(jsonl_image_store_image_path))
     for annotations in data:
-        imgdata = base64.b64decode(annotations['image'].split(',')[1])
+
+        image_byte_stream = b64_uri_to_bytes(annotations["image"])
+        encoded_image_io = io.BytesIO(image_byte_stream)
+        image = Image.open(encoded_image_io)
+
+        filename = str(annotations["meta"]["file"])
+        file_extension = filename.split(".")[-1].lower()
+
+        if file_extension == "png":
+            image_format = '.png'
+        elif file_extension in ("jpg", "jpeg"):
+            image_format = '.jpg'
+        else:
+            logging.info("Only 'png', 'jpeg' or 'jpg' files are supported by ODAPI. "
+                         "Got {}. Thus treating it as `jpg` file. "
+                         "Might cause errors".format(file_extension))
+            image_format = '.jpg'
+
         image_path = os.path.join(cwd,
                                   jsonl_image_store_image_path,
-                                  annotations['text'].split('.')[0] + '.jpg')
-        with open(image_path, 'wb') as f_image:
-            f_image.write(imgdata)
-        height, width = annotations['height'], annotations['width']
+                                  filename.split(".")[0] + image_format)
+
+        # height, width = annotations['height'], annotations['width']
+        width, height = image.size
         if 'spans' in annotations:
             if annotations['answer'] == 'accept':
+                image.save(image_path)
                 for span in annotations['spans']:
-                    span['label'] = span['label'].replace('\ufeff', '')
-                    points = span['points']
+                    # span['label'] = span['label'].replace('\ufeff', '')
+                    points = np.array(span["points"])
+                    xmin, ymin = np.amin(points, axis=0)
+                    xmax, ymax = np.amax(points, axis=0)
+                    # points need to be normalized
+                    xmin = xmin/width
+                    ymin = ymin/height
+                    xmax = xmax/width
+                    ymax = ymax/height
+                    assert xmin < xmax
+                    assert ymin < ymax
+                    # Clip bounding boxes that go outside the image
+                    if xmin < 0:
+                        xmin = 0
+                    if xmax > width:
+                        xmax = width - 1
+                    if ymin < 0:
+                        ymin = 0
+                    if ymax > height:
+                        ymax = height - 1
                     value = (image_path, width, height, span['label'],
-                             int(points[0][0]),
-                             int(points[0][1]),
-                             int(points[2][0]),
-                             int(points[2][1]))
+                             xmin,
+                             ymin,
+                             xmax,
+                             ymax)
                     image_info_list.append(value)
             else:
                 pass
