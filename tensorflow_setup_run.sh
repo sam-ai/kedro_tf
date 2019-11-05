7f824ebb4338db13eac16d741999057a762612b0
sam-ai <34075455+sam-ai@users.noreply.github.com>
2019-11-05 11:52:00 +0530

    Update tensorflow_setup_run.sh

--- tensorflow_setup_run.sh
+++ tensorflow_setup_run.sh
@@ -7,28 +7,28 @@ mv label_map.pbtxt table_classifier/data/04_features/
 mkdir table_classifier/data/02_intermediate/images
 
 git clone https://github.com/sam-ai/object_detection_demo.git
-mv object_detection_demo/ /content/kedro_tf/table_classifier/src/table_classifier/nodes/
+mv object_detection_demo/ table_classifier/src/table_classifier/nodes/
 
 git clone https://github.com/tensorflow/models.git
-mv models/ /content/kedro_tf/table_classifier/src/table_classifier/nodes/
+mv models/ table_classifier/src/table_classifier/nodes/
 
 apt-get install protobuf-compiler python-pil python-lxml python-tk
-pip3 install Cython contextlib2 pillow lxml matplotlib
+pip3 install --user Cython contextlib2 pillow lxml matplotlib
 
-pip3 install pycocotools
+pip3 install --user pycocotools
 
 # shellcheck disable=SC2164
 cd table_classifier/src/table_classifier/nodes/models/research/
 protoc object_detection/protos/*.proto --python_out=.
 
 # shellcheck disable=SC1073
-export PYTHONPATH=':/content/kedro_tf/table_classifier/src/table_classifier/nodes/models/research/:/content/kedro_tf/table_classifier/src/table_classifier/nodes/models/research/slim/'
-# ':$(pwd)/table_classifier/src/table_classifier/nodes/models/research/:$(pwd)/table_classifier/src/table_classifier/nodes/models/research/slim/'
+export PYTHONPATH=':/home/Shiftu-Admin/tfodi_invoice_table_classifier/kedro_tf/table_classifier/src/table_classifier/nodes/models/research/:/home/Shiftu-Admin/tfodi_in$
+# export PYTHONPATH=':$(pwd)/table_classifier/src/table_classifier/nodes/models/research/:$(pwd)/table_classifier/src/table_classifier/nodes/models/research/slim/'
 python3 object_detection/builders/model_builder_test.py
 
 cd ../../../../../
 
-pip3 install kedro
+pip3 install --user kedro
 kedro info
 
 cd table_classifier/
@@ -40,3 +40,4 @@ kedro run -n download_model
 kedro run -n edit_config
 kedro run -n train_model
 kedro run -n store_frozen
+
